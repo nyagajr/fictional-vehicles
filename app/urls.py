b321368da762261b060a50992169f4e64099c7d9
nyagajr <muchokijnrbenson@gmail.com>
2021-10-08 17:53:51 +0300

    routing

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('display',views.display, name='display'),
+    path('',views.display, name='display'),
     path('update/<str:pk>/',views.updateDisplay, name='update'),
-    path('index', views.index, name='index'),
+    path('home', views.index, name='index'),
 
 ]
