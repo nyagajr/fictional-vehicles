b8b80dfa195be7c705b3f2d489d52df0cd53e2a3
nyagajr <muchokijnrbenson@gmail.com>
2021-10-08 17:53:24 +0300

    adding img fields

--- app/models.py
+++ app/models.py
@@ -5,7 +5,10 @@ class CarDetails(models.Model):
     car_make = models.CharField(max_length =30)
     car_model = models.CharField(max_length =30)
     year = models.CharField(max_length = 30)
-    # pic = models.ImageField(upload_to = 'articles/')
+    front_image = models.ImageField(upload_to = 'images/', blank=True)
+    rear_image = models.ImageField(upload_to = 'images/', blank=True)
+    side_image = models.ImageField(upload_to = 'images/', blank=True)
+    interior_image = models.ImageField(upload_to = 'images/', blank=True)
     # post = models.CharField(max_length =30)
     def __str__(self):
         return self.car_make
