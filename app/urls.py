683504c6ce30331959d76fe09fa6b9408992b466
nyagajr <muchokijnrbenson@gmail.com>
2021-10-13 22:46:44 +0300

    form paragraphs

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.display, name='display'),
+    path('/',views.display, name='display'),
+    path('home', views.index, name='home'),
     path('update/<str:pk>/',views.updateDisplay, name='update'),
-    path('home', views.index, name='index'),
+
 
 ]
