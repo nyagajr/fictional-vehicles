4848fe58584e7c416411018dad448879d67b1490
nyagajr <muchokijnrbenson@gmail.com>
2021-10-13 11:10:24 +0300

    modifying the models

--- app/models.py
+++ app/models.py
@@ -5,10 +5,8 @@ class CarDetails(models.Model):
     car_make = models.CharField(max_length =30)
     car_model = models.CharField(max_length =30)
     year = models.CharField(max_length = 30)
-    front_image = models.ImageField(upload_to = 'images/', blank=True)
-    rear_image = models.ImageField(upload_to = 'images/', blank=True)
-    side_image = models.ImageField(upload_to = 'images/', blank=True)
-    interior_image = models.ImageField(upload_to = 'images/', blank=True)
+    front_image = models.ImageField(upload_to = 'images/', blank=True, null=True)
+    rear_image = models.ImageField(upload_to = 'images/', blank=True, null=True)
     # post = models.CharField(max_length =30)
     def __str__(self):
         return self.car_make
