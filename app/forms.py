3c9a51eef9bd91a3642c56092085f82147acd603
nyagajr <muchokijnrbenson@gmail.com>
2021-10-14 13:56:34 +0300

    adding login forms

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
 from django.forms import ModelForm
 from .models import *
 
@@ -5,3 +10,8 @@ class CarDetailsForm(ModelForm):
     class Meta:
         model = CarDetails
         fields = '__all__'
+
+class createUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
