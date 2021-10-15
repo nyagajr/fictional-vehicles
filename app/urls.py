cf5eedfb7a46cde4c46e84da79ddfb46798dc863
nyagajr <muchokijnrbenson@gmail.com>
2021-10-15 10:51:06 +0300

    adding a landing page route

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
-    path('', views.loginPage, name='login'),
-    path('logout/', views.loginPage, name='logout'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 
+    path('',views.landing, name='landing'),
     path('display',views.display, name='display'),
     path('home', views.index, name='home'),
     path('update/<str:pk>/',views.updateDisplay, name='update'),
