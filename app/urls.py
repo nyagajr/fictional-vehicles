91613c61328386d7823badba896f111fc1a6cc6e
nyagajr <muchokijnrbenson@gmail.com>
2021-10-20 08:33:13 +0300

    more urls

--- app/urls.py
+++ app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('display',views.display, name='display'),
     path('home', views.index, name='home'),
     path('update/<str:pk>/',views.updateDisplay, name='update'),
+    path('demo/', views.display_demo, name='demo'),
 
 
 ]
