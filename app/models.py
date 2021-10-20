d5da7b61de16295fc606a1c4160ab0c012bdae52
nyagajr <muchokijnrbenson@gmail.com>
2021-10-20 10:58:42 +0300

    Extending user profile

--- app/models.py
+++ app/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class CarDetails(models.Model):
+class Profile(models.Model):
+    prof_pic = models.ImageField(upload_to = 'images/', blank=True, null=True)
+    bio = models.CharField(max_length = 50)
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+class CarDetails(models.Model):
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     car_make = models.CharField(max_length =30)
     car_model = models.CharField(max_length =30)
     year = models.CharField(max_length = 30)
