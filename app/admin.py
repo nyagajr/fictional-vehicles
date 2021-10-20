cc66a61baf989b3610eb5c01c17627ab790ba89d
nyagajr <muchokijnrbenson@gmail.com>
2021-10-20 10:59:18 +0300

    styling user profile

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from . models import CarDetails
+from . models import *
 
 admin.site.register(CarDetails)
+admin.site.register(Profile)
+
 
 # Register your models here.
